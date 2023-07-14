83948adcf2b744335c539573d92fe777560f0420
jankisojitra <jankisojitra0@gmail.com>
2023-07-14 17:30:02 +0530

    First Commit

---
+++ main.js/custom.js
@@ -0,0 +1,43 @@
+$(".slick_slider").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  speed: 800,
+  prevArrow: '<i class="fa-solid fa-angle-left left_arrow"></i>',
+  nextArrow: '<i class="fa-solid fa-angle-right right_arrow"></i>',
+});
+
+$(".slick_slider3").slick({
+  infinite: false,
+  speed: 800,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  prevArrow: '<i class="fa-solid fa-angle-left left_arrow3"></i>',
+  nextArrow: '<i class="fa-solid fa-angle-right right_arrow3"></i>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
